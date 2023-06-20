a29faf7d8bdd0e37791a38680aa2a3e3f12f8e73
ButenkoDmytro <137228052+ButenkoDmytro@users.noreply.github.com>
2023-06-21 00:21:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x, y;
+    cout << "Project to culculate the sum" << endl;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    cout << "Average: " << (x+y)/2 << endl;
+    return 0;
+}
